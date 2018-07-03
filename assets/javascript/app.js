ba3fe78ff7e0c2dad51bc159bf506579b5155246
Trey Marak <treymarak@alfreds-mbp.attlocal.net>
2018-07-03 12:56:24 -0500

    adding java

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,25 @@
 var answerCorrect = 0;
 var answerIncorrect = 0;
 var unanswered = 0;
+
+var question = ""
+
+function questions(){
+
+var queryURL = "https://opentdb.com/api.php?amount=25&category=29&difficulty=medium&type=multiple";
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
