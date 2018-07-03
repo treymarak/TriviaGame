43dd848384e4dcf41d7dd9e55bb36b42f0a84e93
Trey Marak <treymarak@alfreds-mbp.attlocal.net>
2018-07-03 15:01:00 -0500

    adding api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,21 +2,34 @@ var answerCorrect = 0;
 var answerIncorrect = 0;
 var unanswered = 0;
 
-var question = ""
+var question = [];
+
+var queryURL = "https://opentdb.com/api.php?amount=10&category=29&difficulty=medium&type=multiple";
+
 
 function questions(){
 
-var queryURL = "https://opentdb.com/api.php?amount=25&category=29&difficulty=medium&type=multiple";
+    for (i = 0; i < question.length; i++){
+
+        var arr = question[i];
+        console.log(arr);
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
 
 
 
+});
+
+
+};
 
 
 
-
-
-
-}
+};
 
 
 
