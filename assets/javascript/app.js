2e204a00c6d6491800d7c45936a782c0d4ad42d1
Trey Marak <treymarak@alfreds-mbp.attlocal.net>
2018-07-06 12:33:35 -0500

    adding questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,56 @@
-var answerCorrect = 0;
-var answerIncorrect = 0;
-var unanswered = 0;
+$(document).ready(function() {
 
-var question = [];
+  var index = 0;
+
+  var countdownTimer = {
+
+    time : 30,
+
+    reset: function() {
+
+    this.time = 30;
+
+    $("#timer").html("<h3>" + this.time + "seconds remaining</h3>");
+
+    },
+
+   start: function(){
+
+   counter = setInterval(countdownTimer.count, 1000);
+
+   },
+
+   stop: function(){
+
+   clearInterval(counter);
+
+   },
+
+   count: function(){
+
+   countdownTimer.time--;
+   console.log(countdownTimer.time);
+
+   if (countdownTimer.time >= 0){
+
+    $("#timer").html("<h3>" + this.time + "seconds remaining</h3>");
+}
+
+   else {
+
+   index++;
+   
+   countdownTimer.reset();
+
+
+   }
+
+   }
+
+   
+
+  }
+
 
 var queryURL = "https://opentdb.com/api.php?amount=10&category=29&difficulty=medium&type=multiple";
 
@@ -14,21 +62,14 @@ function questions(){
       method: "GET"
     }).then(function(response) {
       console.log(response);
+      console.log(response.results[0].question);
     });
-
-    for (i = 0; i < question.length; i++){
-
-        var arr = question[i];
-        console.log(arr);
-        
-    }
-
     
 
 };
 
 questions();
 
-
+});
 
 
