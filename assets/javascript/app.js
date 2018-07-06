8a7be7a68c90cec786dde0c7595bbcccc43e29fb
Trey Marak <treymarak@alfreds-mbp.attlocal.net>
2018-07-06 16:57:51 -0500

    fixing bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,9 +53,12 @@ $(document).ready(function() {
 
 
 var queryURL = "https://opentdb.com/api.php?amount=10&category=29&difficulty=medium&type=multiple";
-
+var correct = 0;
+var incorrect = 0;
 
 function questions(){
+
+  countdownTimer.reset();
     
     $.getJSON({
       url: queryURL,
@@ -63,13 +66,66 @@ function questions(){
     }).then(function(response) {
       console.log(response);
       console.log(response.results[0].question);
+      console.log(response.results[0].correct_answer);
+      console.log(response.results[0].incorrect_answers);
+
+      var trivia = response.results[0].question;
+      var answer = response.results[0].correct_answer;
+      
+
+      $("#inquiry").text(trivia);
+      $("#option1").text(answer);
+      $("#option2").text(response.results[0].incorrect_answers[0]);
+      $("#option3").text(response.results[0].incorrect_answers[1]);
+      $("#option4").text(response.results[0].incorrect_answers[2]);
+
+      
+
     });
     
 
 };
 
-questions();
+
 
 });
 
+
+
+function setup(){
+
+index = 0;
+
+$("#inquiry").append("<button id='startButton'>Start</button>");
+$("#startButton").on("click", function(){
+
+  $(this).hide();
+  countdownTimer.start();
+  questions(index);
+
+});
+
+
+}
+
+function getAnswer(){
+
+$(".options").on("click", function(){
+
+index++;
+
+$("#inquiry").text('');
+		$("#option1").text('');
+		$("#option2").text('');
+		$("#option3").text('');
+    $("#option4").text('');
+    
+   questions();
+
+});
+
+
+
+
+}
 
